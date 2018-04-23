e3cd8af9ce77e29f8907593b9d35e30d1419f92e
clbokea <clbo@kea.dk>
2018-04-23 10:53:03 +0200

    færdigt

--- Account.java
+++ Account.java
@@ -1,6 +1,7 @@
 public class Account {
 
    private int accountNumber;
+   private int balance;
 
    public Account(int accountNumber){
       this.accountNumber = accountNumber;
@@ -9,10 +10,25 @@ public class Account {
    public boolean verifyPin(int cardnumber, int pin){
      
      // TODO: implementer Kode!!!!
-     
       return true;
    } 
    
-   public void   
+   public int withdraw(int amount){   
+         
+         int result = debit(amount);
+         return amount;
+   }
+   
+   private int debit(int amount){
+        
+        if(balance - amount >= 0){
+            return amount;      
+        }else {
+           
+           return -1;
+         
+        }      
+   }
+   
 
 }
--- Atm.java
+++ Atm.java
@@ -2,26 +2,37 @@ import java.util.Scanner;
 
 public class Atm {
 
-         private static Scanner scan = new Scanner(System.in);
+   private static Scanner scan = new Scanner(System.in);
 
    public static void main(String[] args){
-         
-              // Insert ATM card
-              // Ask for pin
-              // type pin  
+       
+      // TODO: Kode til:  
+      // Insert ATM card
+      // Ask for pin
+      // type pin  
               
       Accounts accounts = new Accounts();
       Account account = accounts.getAccount(1);
               
       if(!account == null) { 
-          boolean ok = account.verifyPin(1, 1234);
+         
+         boolean ok = account.verifyPin(1, 1234);
            
-           if (ok == true){
-                  // spørg efter beløb
-                  // Kunde indtaster beløb
-                  int amount = scan.nextInt();
-                  account.withdraw(amount);
-           }
+         if (ok == true){
+            // spørg efter beløb
+            // Kunde indtaster beløb
+            int amount = scan.nextInt();
+            int result = account.withdraw(amount);
+            
+            // giv kort tilbage
+            
+            if (result != -1) {
+                  System.out.println("Her er dine penge: " + result);
+            } else {
+                  System.out.println("Du har ikke nok penge !! Buuhhh");
+            }
+            
+         }
                   
       } else {
          // quit
